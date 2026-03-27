09f79edcf11f506642159c54e7c539c264f5c432
junseoyang902-cyber <junseoyang902@gmail.com>
2026-03-28 01:16:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.set_page_config(page_title="리액위키", page_icon="🎭")
 
 st.title("🎭 리액위키")
-st.caption("모든 데이터의 출처는 [리버액트 역대 공연 연혁](https://slowdemoc.notion.site/dd6b64eea8784adebb3363d6db65d591?source=copy_link) 입니다.")
+st.caption("모든 데이터의 출처는 [리버액트 역대 공연 연혁](https://slowdemoc.notion.site/dd6b64eea8784adebb3363d6db65d591?source=copy_link) 페이지입니다.")
 st.caption("🛠️ made by 양준서")
 
 # ---------------------------
