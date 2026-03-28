ab6374a549957399ffa6f9162f1d8a258e59ae55
junseoyang902-cyber <junseoyang902@gmail.com>
2026-03-28 14:48:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -92,20 +92,20 @@ if menu == "부원별 활동 기록 보기":
 elif menu == "공연별 참여 부원 보기":
 
     def sort_leader(row):
-    role = str(row["역할"])
+        role = str(row["역할"])
 
-    if "연출" in role:
-        return 0
-    elif "조연출" in role:
-        return 0.5
-    elif any(x in role for x in ["연기감독", "연기지도", "연기고문", "연기부연출"]):
-        return 1
-    elif any(x in role for x in ["미술감독", "미술부연출"]):
-        return 2
-    elif "기획" in role:
-        return 3
-    else:
-        return 4
+        if "연출" in role:
+            return 0
+        elif "조연출" in role:
+            return 0.5
+        elif any(x in role for x in ["연기감독", "연기지도", "연기고문", "연기부연출"]):
+            return 1
+        elif any(x in role for x in ["미술감독", "미술부연출"]):
+            return 2
+        elif "기획" in role:
+            return 3
+        else:
+            return 4
     
     # ---------------------------
     # 📌 카테고리 분류
